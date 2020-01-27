c8962d84a52ef4d180236ca35983ef247fd96f1b
Konstantin Schuckmann <kn.schuckmann@gmx.de>
2020-01-27 10:44:48 +0100

    - deleted files that did not work properly - now there is a fully functioning windows qr generator / creator executable for either csv or excel files

--- qr_creator.py
+++ qr_creator.py
@@ -13,6 +13,8 @@ import qrcode
 import tkinter as tk
 from tkinter import filedialog
 
+import sys
+
 import pandas as pd
 
 #%%%
@@ -22,20 +24,24 @@ root.withdraw()
 
 endung = 'keine'
 
+print('Öffne die gesuchte Datei!\nAchte darauf, dass diese entweder als xlsx oder csv vorliegt!\n\n')
+
+
 while endung == 'keine':
     
     file_path = filedialog.askopenfilename()
     if not file_path:
-        break
-    if file_path[-3:] == 'csv':
-        raw_data = pd.read_csv(file_path, head)
-        endung = 'csv'
-    elif file_path[-4:] == 'xlsx':
+        sys.exit('Programm wurde vom Benutzer beendet')
+    if file_path[-4:] == 'xlsx':
         raw_data = pd.read_excel(file_path)
-        endung = 'xlsx'
+        endung = 'xlsx'        
+    elif file_path[-3:] == 'csv':
+        raw_data = pd.read_csv(file_path, head)
+        endung = 'csv'        
     else:
         endung = 'keine'
         print('Bitte formatiere deine Datei in xlsx oder csv Datei um!\n\nDeine Datei muss außerdem eine Spalte mit Namen: Buchungsnummer besitzen.\n\n\n')
+    
 #%%
         
 print('Wo willst du die Dateien speichern?\nGib hier bitte den Pfad an!\n')
@@ -44,7 +50,7 @@ root.withdraw()
 
 file_path = filedialog.askdirectory()
 if not file_path:
-   exit(0)
+   sys.exit('Programm wurde vom Benutzer beendet')
 
 #%%
 # Preprocessing
