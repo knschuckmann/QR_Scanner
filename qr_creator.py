cdfbe54b382f93b20f72f82b177803c95bc84825
Konstantin Schuckmann <kn.schuckmann@gmx.de>
2020-12-18 10:10:21 +0100

    some minor changes in main file

--- qr_creator.py
+++ qr_creator.py
@@ -56,10 +56,33 @@ if not file_path:
    sys.exit('Programm wurde vom Benutzer beendet')
 
 #%%
-
-
+new_df = raw_data
+new_df['Bildname'] = ''
+new_df['QR-Code'] = ''
 for i, nr in enumerate(buchungsnummer):
     img = qrcode.make(nr, border = 2, box_size = 10, error_correction=qrcode.constants.ERROR_CORRECT_Q)
+    new_df['Bildname'][i] = file_path + '/' + nr + ".png"
     img.save(file_path + '/'+ nr + ".png")
-   
+    new_df['QR-Code'][i] = img
+    new_df['QR-Code'] = new_df.Bildname.map(lambda f: get_thumbnail(file_path + '/' + f))
+    
+
+#%%
+def get_thumbnail(path):
+    i = Image.open(path)
+    i.thumbnail((150, 150), Image.LANCZOS)
+    return i
+
+def image_base64(im):
+    if isinstance(im, str):
+        im = get_thumbnail(im)
+    with BytesIO() as buffer:
+        im.save(buffer, 'jpeg')
+        return base64.b64encode(buffer.getvalue()).decode()
+
+def image_formatter(im):
+    return f'<img src="data:image/jpeg;base64,{image_base64(im)}">'
+
+#%%
+new_df.to_excel(file_path + '/neie2.xlsx')
     
